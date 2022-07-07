dd2fd653523c21aa4302558e1505182f2562f029
Shrawan Bagariya <skbagariya63@gmail.com>
2022-07-07 19:28:57 +0530

    JSX code practice

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
     <div>
         <h1>
-            Hello World!
+            Great Forts of Rajasthan
         </h1>
+        <ul>
+            <li>Chittorgarh Fort - Chittorgarh</li>
+            <li>Kumbhalgarh Fort - Udaipur</li>
+            <li>Golden Fort - Jaisalmer</li>
+            <li>Amer Fort - Jaipur</li>
+            <li>Meharangarh Fort - Jodhpur</li>
+        </ul>
     </div>,
     document.getElementById("root")
 );
