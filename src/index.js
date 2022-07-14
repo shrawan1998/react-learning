411b7314b637af733a0cb1f478b7974c4edf98f9
Shrawan Bagariya <skbagariya63@gmail.com>
2022-07-14 13:00:44 +0530

    JSX attributes & styling react elements part-2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style.css"
+import "./style.css";
 
 const name = "Shrawan";
 const year = new Date().getFullYear();
 ReactDOM.render(
     <div>
-        <h1 className="heading" contentEditable="true" spellCheck="false">Great Forts of Rajasthan</h1>
-        <ul>
-            <li>Chittorgarh Fort - Chittorgarh</li>
-            <li>Kumbhalgarh Fort - Udaipur</li>
-            <li>Golden Fort - Jaisalmer</li>
-            <li>Amer Fort - Jaipur</li>
-            <li>Meharangarh Fort - Jodhpur</li>
-        </ul>
+        <h1 className="heading">Great Forts of Rajasthan</h1>
+        <div className="fort-imgs">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSIKUbjrjsN00dBurCkkwY6VQSh6uqj4pKXQ&usqp=CAU" alt="Chittorgarh Fort - Chittorgarh"/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyFYVyly1ImRWrklVDOLtIDJ4kPqvcKxYNiA&usqp=CAU" alt="Kumbhalgarh Fort - Udaipur"/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUMzR4D198O0Lrj9eA4Wo3x-Ww6Mew9P9r-Q&usqp=CAU" alt="Meharangarh Fort - Jodhpur"/>
+        </div>
         <p>Created by {name} & Copyright {year} </p>
     </div>,
     document.getElementById("root")
