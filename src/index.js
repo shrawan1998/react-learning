58c21e55598a94a351b8bd8562a2651264f66eb7
Shrawan Bagariya <skbagariya63@gmail.com>
2022-07-14 12:19:47 +0530

    JSX attributes & styling react elements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css"
 
 const name = "Shrawan";
 const year = new Date().getFullYear();
 ReactDOM.render(
     <div>
-        <p>Created by {name} </p>
-        <p>Copyright {year} </p>
+        <h1 className="heading" contentEditable="true" spellCheck="false">Great Forts of Rajasthan</h1>
+        <ul>
+            <li>Chittorgarh Fort - Chittorgarh</li>
+            <li>Kumbhalgarh Fort - Udaipur</li>
+            <li>Golden Fort - Jaisalmer</li>
+            <li>Amer Fort - Jaipur</li>
+            <li>Meharangarh Fort - Jodhpur</li>
+        </ul>
+        <p>Created by {name} & Copyright {year} </p>
     </div>,
     document.getElementById("root")
 );
