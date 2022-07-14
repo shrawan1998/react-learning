5509d07095dc7420b366e9842ed339f3764985dc
Shrawan Bagariya <skbagariya63@gmail.com>
2022-07-14 13:17:40 +0530

    SX attributes & styling react elements part-3

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import "./style.css";
 
 const name = "Shrawan";
 const year = new Date().getFullYear();
+const img = "https://picsum.photos/200";
 ReactDOM.render(
     <div>
-        <h1 className="heading">Great Forts of Rajasthan</h1>
-        <div className="fort-imgs">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSIKUbjrjsN00dBurCkkwY6VQSh6uqj4pKXQ&usqp=CAU" alt="Chittorgarh Fort - Chittorgarh"/>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyFYVyly1ImRWrklVDOLtIDJ4kPqvcKxYNiA&usqp=CAU" alt="Kumbhalgarh Fort - Udaipur"/>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUMzR4D198O0Lrj9eA4Wo3x-Ww6Mew9P9r-Q&usqp=CAU" alt="Meharangarh Fort - Jodhpur"/>
-        </div>
+        <h1 className="heading">Lorem picsum images</h1>
+        <img src={img} alt="" />
+        <img src={img + "?grayscale"} alt="" />
         <p>Created by {name} & Copyright {year} </p>
     </div>,
     document.getElementById("root")
