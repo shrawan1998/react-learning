fd0f1c5743e376bb9090391a967945c0c00495bf
Shrawan Bagariya <skbagariya63@gmail.com>
2022-07-14 16:32:48 +0530

    React styling practice

--- src/index.js
+++ src/index.js
@@ -2,18 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-const customStyle = {
-    color: "blue",
-    border: "1px solid black",
-    fontSize: "20px",
-};
+const currentTime = new Date().getHours();
 
-customStyle.border = "1px dashed green";
+const customStyle = {
+    color: "",
+    borderBottom: "",
+}
+
+let greetings;
+
+if (currentTime > 5 && currentTime < 12) {
+    greetings = "Good Morning!!";
+    customStyle.color = "Green";
+    customStyle.borderBottom = "3px solid green";
+} else if(currentTime > 12 && currentTime < 15) {
+    greetings = "Good Afternoon!!";
+    customStyle.color = "yellow";
+    customStyle.borderBottom = "3px solid yellow";
+} else if(currentTime > 15 && currentTime < 21) {
+    greetings = "Good Evening!!";
+    customStyle.color = "blue";
+    customStyle.borderBottom = "3px solid blue";
+} else{
+    greetings = "Good Night!!";
+    customStyle.color = "red";
+    customStyle.borderBottom = "3px solid red";
+}
 
 ReactDOM.render(
-    <div>
-        <p style={{color: "red", fontSize: "20px"}}>Hello World!</p>
-        <p style={customStyle} >Hello World!</p>
-    </div>,
+    <h1 className="greet-message" style={customStyle}>{greetings} </h1>,
     document.getElementById("root")
 );
