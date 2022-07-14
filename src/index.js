ef860bd0da976a5809821de29f08a73a4974744a
Shrawan Bagariya <skbagariya63@gmail.com>
2022-07-14 15:36:30 +0530

    Inline styling for react elements

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-const name = "Shrawan";
-const year = new Date().getFullYear();
-const img = "https://picsum.photos/200";
+const customStyle = {
+    color: "blue",
+    border: "1px solid black",
+    fontSize: "20px",
+};
+
+customStyle.border = "1px dashed green";
+
 ReactDOM.render(
     <div>
-        <h1 className="heading">Lorem picsum images</h1>
-        <img src={img} alt="" />
-        <img src={img + "?grayscale"} alt="" />
-        <p>Created by {name} & Copyright {year} </p>
+        <p style={{color: "red", fontSize: "20px"}}>Hello World!</p>
+        <p style={customStyle} >Hello World!</p>
     </div>,
     document.getElementById("root")
 );
